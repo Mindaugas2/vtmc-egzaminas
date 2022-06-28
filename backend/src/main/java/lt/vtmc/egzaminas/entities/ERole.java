451e3e877905b1fb74d3a39bc988ede05df2e020
Julius <jzabulenas@protonmail.com>
2022-06-28 10:30:52 +0300

    code 'User' and 'Role' entity classes, which ultimately helps Spring Data JPA create these represenations as tables in the database

---
+++ backend/src/main/java/lt/vtmc/egzaminas/entities/ERole.java
@@ -0,0 +1,8 @@
+package lt.vtmc.egzaminas.entities;
+
+public enum ERole {
+
+	ROLE_USER,
+	ROLE_MODERATOR,
+	ROLE_ADMIN
+}
--- backend/src/main/java/lt/vtmc/egzaminas/entities/User.java
+++ backend/src/main/java/lt/vtmc/egzaminas/entities/User.java
@@ -1,5 +1,106 @@
 package lt.vtmc.egzaminas.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	private String firstName;
+
+	@NotBlank
+	private String lastName;
+
+	@NotBlank
+	@Email
+	private String email;
+
+	@JsonIgnore
+	@NotBlank
+	private String password;
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
+	public User() {
+
+	}
+
+	public User(@NotBlank String firstName, @NotBlank String lastName, @NotBlank @Email String email,
+			@NotBlank String password) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
 
 }
